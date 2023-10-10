455abd23a79b8a2ab64bc06130cf1dc5f4f8d288
christarday <137360488+christarday@users.noreply.github.com>
2023-10-10 18:14:42 +0000

    question a midterm

--- src/question_b/question_b.py
+++ src/question_b/question_b.py
@@ -1,1 +1,14 @@
 #write functions here, don't add input('') statements here!
+# question_b.py)
+global_var = 5
+
+# Function to modify the global variable
+def use_global():
+    global global_var  # Declare that we want to use the global variable
+    global_var += 10  # Modify the global variable by adding 10
+
+# Call the function to modify the global variable
+use_global()
+
+# Display the updated value of the global variable
+print("Updated global_var:", global_var)
